77845c05e8c24feafacb831d0e5808988318e628
shibamoto <silverbirder@gmail.com>
2023-06-26 23:32:34 +0900

    abcd testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useFlag } from "@unleash/proxy-client-react";
+import { useFlag, useVariant } from "@unleash/proxy-client-react";
 
 function App() {
   const featureName =
     new URLSearchParams(window.location.search).get("feature") || "";
-  const enabled = useFlag(featureName);
-
+  const flag = useFlag(featureName);
+  const variant = useVariant(featureName);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {enabled && <>{featureName}</>}
+        <div>{flag && featureName}</div>
+        <div>{variant.enabled && variant.name}</div>
       </header>
     </div>
   );
